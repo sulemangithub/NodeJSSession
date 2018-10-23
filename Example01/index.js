1f3465ad4d91cb19f5865ea8ac568a94e2fcc9d7
Suleman Soudagar <sulemans@kpit.com>
2018-10-23 16:39:56 +0530

    added synchronous and asynchronous examples

--- Example01/index.js
+++ Example01/index.js
@@ -1,1 +1,20 @@
-console.log("This is example 1");
+
+function order(orderNumber)
+{
+    console.log("Order Received....."+orderNumber);
+    deliver(orderNumber);
+}
+
+
+function deliver(orderNumber)
+{
+    console.log("Order Delivered....."+orderNumber);
+}
+
+order(1);
+order(2);
+order(3);
+order(4);
+order(5);
+order(6);
+order(7);
--- Example02/index.js
+++ Example02/index.js
@@ -1,1 +1,19 @@
-console.log("This is example 2");
+
+function order(orderNumber,cb)
+{
+    console.log("Order Received..."+orderNumber);
+
+    setTimeout(function(){
+        cb(orderNumber);
+    },1000);
+}
+
+
+function deliver(orderNumber)
+{
+    console.log("Order Delivered..."+orderNumber);
+}
+
+order(1,deliver);
+order(2,deliver);
+order(3,deliver);
